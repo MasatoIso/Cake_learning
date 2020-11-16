3b1eb263abd2cd4391e1d5c94220ade9913bf524
MasatoIso <04masato05@gmail.com>
2020-11-17 08:04:40 +0900

    create add action

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class ArticlesController extends AppController
+{
+    public function initialize()
+    {
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
+    public function index()
+    {
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+
+    }
+}
