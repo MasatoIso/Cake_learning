dc719ce41fefa4a845789503d525dc01d2368f0b
MasatoIso <04masato05@gmail.com>
2020-11-21 09:00:06 +0900

    create Tags Users

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -38,13 +38,18 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
+        $tags = $this->Articles->Tags->find('list');
+        $this->set('tags', $tags);
         $this->set('article', $article);
 
     }
 
     public function edit($slug)
     {
-        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->contain('Tags')
+            ->firstOrFail();
         if ($this->request->is(['post', 'put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
@@ -53,7 +58,8 @@ class ArticlesController extends AppController
             }
             $this->Flash->error(__('Unable to add your article.'));
         }
-
+        $tags = $this->Articles->Tags->find('list');
+        $this->set('tags', $tags);
         $this->set('article', $article);
     }
 
@@ -65,6 +71,19 @@ class ArticlesController extends AppController
         if ($this->Articles->delete($article)) {
             $this->Flash->success(__('The {0} article has been deleted.', $article->title));
             return $this->redirect(['action' => 'index']);
-        }1
+        }
+    }
+
+    public function tags()
+    {
+        $tags = $this->request->getParam('pass');
+        $articles  = $this->Articles->find('tagged', [
+            'tags' => $tags
+        ]);
+
+        $this->set([
+            'articles' => $articles,
+            'tags' => $tags
+        ]);
     }
 }
